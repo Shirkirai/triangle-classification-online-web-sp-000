9516c1e9f1dc46a2a2d5541fbd4f18712c85c42e
Jung Lee <lee.jung.s95@gmail.com>
2020-06-09 03:06:15 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,9 +2,8 @@ class Triangle
   attr_accessor :equilateral, :isosceles, :scalene
 
   def initialize(s1, s2, s3)
-    if s1 == s2 && s1 == s3 && s2 == s3
-      :equilateral
-    end
+    
+    
   end
 
   def kind
