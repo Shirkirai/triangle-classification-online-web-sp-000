7e9086320a0bee4e7c47110933beba68e2fcf420
Jung Lee <lee.jung.s95@gmail.com>
2020-06-09 03:02:49 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,7 +1,7 @@
 class Triangle
   attr_accessor :equilateral, :isosceles, :scalene
 
-  def initialize(1, 2, 3)
+  def initialize(s1, s2, s3)
     if 1 == 2 && 1 == 3 && 2 == 3
       equilateral
     end
